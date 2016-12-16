89c5232d347d8be2c7fb4abecc141d9614440d46
John Downey <jdowney@gmail.com>
2016-12-15 20:10:57 -0600

    Add day 1

---
+++ src/common/lib.rs
@@ -0,0 +1,41 @@
+use errors::*;
+use std::env;
+use std::str::FromStr;
+
+#[macro_use]
+extern crate error_chain;
+
+pub mod errors {
+    error_chain!{
+        foreign_links {
+            Io(::std::io::Error);
+            ParseInt(::std::num::ParseIntError);
+            Utf8(::std::str::Utf8Error);
+        }
+    }
+}
+
+pub enum Problem {
+    Part1,
+    Part2,
+}
+
+impl Problem {
+    pub fn from_arg(arg: usize) -> Result<Problem> {
+        env::args()
+            .nth(arg)
+            .ok_or_else(|| "No part specified".into())
+            .and_then(|v| v.parse())
+    }
+}
+
+impl FromStr for Problem {
+    type Err = Error;
+    fn from_str(part: &str) -> Result<Problem> {
+        match part {
+            "1" => Ok(Problem::Part1),
+            "2" => Ok(Problem::Part2),
+            _ => bail!("Unable to parse {}", part),
+        }
+    }
+}
